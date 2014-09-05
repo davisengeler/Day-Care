15ea250d9750b36d89844f9d14994d52e635b754
ianuilliam <ianuilliam@gmail.com>
2014-09-05 10:54:42 -0400

    Don't stop believing

---
+++ 540 - Day Care/src/Communications.java
@@ -0,0 +1,7 @@
+
+public class Communications {
+	//Handles sending parent-teacher messages
+	public void sendMessage(String message, String recipient){
+		//Send message to recipient. or something.
+	}
+}
