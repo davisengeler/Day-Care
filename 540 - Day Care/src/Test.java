f51ddaf59cecef4c3ec84e828ca1d44c96b52b45
ianuilliam <ianuilliam@gmail.com>
2014-09-05 10:38:42 -0400

    the commits are real

--- 540 - Day Care/src/Test.java
+++ 540 - Day Care/src/Test.java
@@ -8,6 +8,7 @@ public class Test
 		
 		System.out.println("This is a test for GitHub. If you see this, things are working well.");
 		System.out.println("Edited by Michael.");
+		System.out.print("\n Don't forget mee\n                 e\n                  e\n                   e");
 	}
 
 }
