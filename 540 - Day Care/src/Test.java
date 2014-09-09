aef59736ee25131be05de4149a126fd2f0287748
Davis Engeler <davisengeler+github@gmail.com>
2014-09-09 19:45:30 -0400

    Added a comment line for Jesse in Test.java

--- 540 - Day Care/src/Test.java
+++ 540 - Day Care/src/Test.java
@@ -9,6 +9,8 @@ public class Test
 		System.out.println("This is a test for GitHub. If you see this, things are working well.");
 		System.out.println("Edited by Michael.");
 		System.out.print("\n Don't forget mee\n                 e\n                  e\n                   e");
+		
+		// Jesse: Add a println below here and push the change up to GitHub to make sure it works.
 	}
 
 }
