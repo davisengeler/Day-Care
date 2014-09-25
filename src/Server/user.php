58430a9cf81557e9c2afa11eb081b8f51956172c
Davis Engeler <davisengeler+github@gmail.com>
2014-09-25 09:00:50 -0400

    Added example in the user.php for server

--- src/Server/user.php
+++ src/Server/user.php
@@ -1,1 +1,15 @@
 // API for user functions: registration, user info, permissions, logins, etc
+
+$first = $_GET["first"];
+$last = $_GET["last"];
+$SSN = $_GET["ssn"];
+
+echo $first . "'s API key is " . create_user($first, $last, "", "", "", $SSN, "");
+
+function create_user($first, $last, $email, $phone, $birthday, $SSN, $accountType)
+{
+	$userID = $last . $first;
+	$apiKey = md5($userID);
+	
+	return $apiKey;
+}
