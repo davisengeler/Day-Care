20e4d55c350dbcaa09f121d6a934b568570702ba
Davis Engeler <davisengeler+github@gmail.com>
2014-10-16 14:42:21 -0400

    Continuing to set up environment

--- src/Server/config.php
+++ src/Server/config.php
@@ -2,7 +2,7 @@
 
   // Database Configuration
 
-  define("DB_NAME", "davisengeler_daycare");
+  define("DB_NAME", "davisengeler_daycare_WRONGDELETEME");
   define("DB_HOST", "localhost");
   define("DB_USER", "daycare");
   define("DB_PASS", "dysd*&6sf8ll'");
