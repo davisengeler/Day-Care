d649cb92e60d2d717bb4a195fa343dc6f591b8bc
Davis Engeler <davisengeler+github@gmail.com>
2014-10-16 14:22:26 -0400

    Updated add-device.php

--- src/Server/add-device.php
+++ src/Server/add-device.php
@@ -6,6 +6,8 @@
   // Connect to DB. Comes from default-functions.php
   db_connect();
 
+  echo "HEY THERE";
+
   $deviceID = $_GET["deviceID"];
 
   mysql_query("INSERT INTO Devices(DeviceID) VALUES ($deviceID)");
