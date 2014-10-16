d1638a81747e916b29139c0cd027bdb9d8a77220
Davis Engeler <davisengeler+github@gmail.com>
2014-10-16 14:37:47 -0400

    Continuing to set up environment

--- src/Server/add-device.php
+++ src/Server/add-device.php
@@ -4,18 +4,13 @@
   include("default-functions.php");
 
   // Connect to DB. Comes from default-functions.php
-  $con = mysql_connect(DB_HOST,DB_USER,DB_PASS);
-  if (!$con)
-  {
-    die('Could not connect to database: ' . mysql_error());
-  }
-  mysql_select_db(DB_NAME, $con);
+  db_connect();
 
   echo "HEY THEREs";
 
   $deviceID = $_GET["deviceID"];
 
-  if(mysql_query("INSERT INTO Devices(DeviceID) VALUES ($deviceID)"))
+  if(mysql_query("INSERT INTO Devices(DeviceID) VALUES ($deviceID);"))
   {
     echo "SHIT'S DONE";
   }
