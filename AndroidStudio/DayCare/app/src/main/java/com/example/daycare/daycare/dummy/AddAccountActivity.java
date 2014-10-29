bd0f2b564b31c9ca3d175e15e75c120099e0732a
mehetzel <hetzelm@email.uscupstate.edu>
2014-10-29 13:50:25 -0400

    Adding button listener for add account

--- AndroidStudio/DayCare/app/src/main/java/com/example/daycare/daycare/dummy/AddAccountActivity.java
+++ AndroidStudio/DayCare/app/src/main/java/com/example/daycare/daycare/dummy/AddAccountActivity.java
@@ -4,10 +4,15 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
 import com.example.daycare.daycare.R;
 
 public class AddAccountActivity extends Activity {
 
+    Button submitButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -32,5 +37,36 @@ public class AddAccountActivity extends Activity {
             return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    public void addListenerButton()
+    {
+        final EditText fName = (EditText) findViewById(R.id.first_name);
+        final EditText lName = (EditText) findViewById(R.id.last_name);
+        final EditText sAddress = (EditText) findViewById(R.id.street_address);
+        final EditText sCity = (EditText) findViewById(R.id.city);
+        final EditText sState = (EditText) findViewById(R.id.state);
+        final EditText sZip = (EditText) findViewById(R.id.zip);
+        final EditText emailAddress = (EditText) findViewById(R.id.email);
+        final EditText pNum = (EditText) findViewById(R.id.phone);
+
+
+        submitButton = (Button) findViewById(R.id.submitButton);
+        submitButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //take the info and send to php
+                String firstName, lastName, street, city, state, zip, email, phone;
+                firstName = fName.getText().toString();
+                lastName = lName.getText().toString();
+                street = sAddress.getText().toString();
+                city = sCity.getText().toString();
+                state = sState.getText().toString();
+                zip = sZip.getText().toString();
+                email = emailAddress.getText().toString();
+
+
+            }
+        });
     }
 }
