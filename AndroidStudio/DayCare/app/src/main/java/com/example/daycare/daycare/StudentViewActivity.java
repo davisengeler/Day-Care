181f0f8f5550c1bec12a7dbe0ea417eea355322a
mehetzel <hetzelm@email.uscupstate.edu>
2014-11-02 17:04:07 -0500

    added popup dialogs

--- AndroidStudio/DayCare/app/src/main/java/com/example/daycare/daycare/StudentViewActivity.java
+++ AndroidStudio/DayCare/app/src/main/java/com/example/daycare/daycare/StudentViewActivity.java
@@ -1,23 +1,58 @@
 package com.example.daycare.daycare;
 
-import android.app.ListActivity;
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.app.DialogFragment;
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ListView;
 
 
-public class StudentViewActivity extends ListActivity {
-    String [] actions = {"Contact Parent", "Add Note", "Move Student"};
-
+public class StudentViewActivity extends Activity {
+    protected String [] actions = {"Contact Parent", "Add Note", "Move Student"};
+    static final String [] teachers = {"Jane Smith", "John Doe", "Davis Engeler", "John Sloan", "Michael Hetzel"};
+    private ListView mListView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_student_view);
 
-        ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, actions);
-        setListAdapter(adapter);
         String chosenStudent = this.getIntent().getStringExtra("chosenStudent");
+
+        mListView = (ListView) findViewById(R.id.container);
+        ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, actions);
+        mListView.setAdapter(adapter);
+        mListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                switch (i)
+                {
+                    case 0:
+                        //for testing purposes
+                        DialogFragment dgE = new ErrorMessageDialog();
+                        dgE.show(getFragmentManager(), "error");
+                        break;
+                    case 1:
+                        DialogFragment dg2 = new NoteDialogFragment();
+                        dg2.show(getFragmentManager(), "notes");
+                        break;
+                    case 2:
+                        DialogFragment dg1 = new TeacherDialogFragment();
+                        dg1.show(getFragmentManager(), "teachers");
+                        break;
+                    default:
+                        break;
+                }
+            }
+        });
     }
 
 
@@ -41,5 +76,80 @@ public class StudentViewActivity extends ListActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+    public static class TeacherDialogFragment extends DialogFragment
+    {
+        public Dialog onCreateDialog(Bundle savedInstanceState)
+        {
+            AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+            builder.setTitle(R.string.pick_teacher)
+                    .setItems(teachers, new DialogInterface.OnClickListener()
+                    {
+                        public void onClick(DialogInterface dialog, int which)
+                        {
+                            // The 'which' argument contains the index position
+                            // of the selected item
+                        }
+                    }
+                    );
+            return builder.create();
+        }
+    }
+
+    public static class NoteDialogFragment extends DialogFragment
+    {
+        String[] noteID = {"Meal", "Nap", "Accident", "Needs", "Misc"};
+        int noteIDChosen =4;
+        public Dialog onCreateDialog(Bundle savedInstanceState)
+        {
+
+            AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+            final EditText message = new EditText(getActivity());
+            message.setHint("Enter Message Here");
+            builder.setTitle(R.string.add_note_title)
+                    .setSingleChoiceItems(noteID, noteIDChosen, new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialogInterface, int i) {
+                            noteIDChosen = i;
+                        }
+                    })
+                    .setView(message)
+                    .setPositiveButton(R.string.save_label, new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialogInterface, int i) {
+
+                            String mContent = message.getText().toString();
+                            Log.v("NoteMessage", noteIDChosen + " " + mContent);
+                            //submit this as a note to server with noteIDChosen
+                        }
+                    })
+                    .setNegativeButton(R.string.cancel_label, new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialogInterface, int i) {
+                            NoteDialogFragment.this.getDialog().cancel();
+                        }
+                    });
+            return builder.create();
+        }
+    }
+
+    public static class ErrorMessageDialog extends DialogFragment
+    {
+        public Dialog onCreateDialog(Bundle savedInstanceState)
+        {
+            AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+            builder.setTitle("Must select a note type")
+                    .setMessage("Please select the type of note")
+                    .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialogInterface, int i) {
+
+                        }
+                    })
+                    .setIcon(android.R.drawable.ic_dialog_alert);
+            return builder.create();
+        }
+
+
     }
 }
