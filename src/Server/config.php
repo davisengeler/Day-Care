bec64113bceb1ba42926d90988c780904a66a70d
Davis Engeler <davisengeler+github@gmail.com>
2014-11-18 14:29:48 -0500

    Updated database connection setup.

--- src/Server/config.php
+++ src/Server/config.php
@@ -7,4 +7,10 @@
   define("Database_USER", "daycare");
   define("Database_PASS", "engelerhetzel");
 
+  function connectDB()
+  {
+    $connection = mysqli_connect(Database_HOST, Database_USER, Database_PASS, Database_NAME) or die("Could not connect to database");
+    return $connection;
+  }
+
 ?>
