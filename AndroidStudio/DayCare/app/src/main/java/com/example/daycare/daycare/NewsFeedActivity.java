0a8e1ebc731d8ddce26cbc785000b66a942808cc
mehetzel <hetzelm@email.uscupstate.edu>
2014-11-18 23:33:24 -0500

    Sign in and out COMPLETE

--- AndroidStudio/DayCare/app/src/main/java/com/example/daycare/daycare/NewsFeedActivity.java
+++ AndroidStudio/DayCare/app/src/main/java/com/example/daycare/daycare/NewsFeedActivity.java
@@ -4,6 +4,10 @@ import android.animation.Animator;
 import android.animation.AnimatorListenerAdapter;
 import android.annotation.TargetApi;
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.app.DialogFragment;
+import android.content.DialogInterface;
 import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Build;
@@ -18,6 +22,7 @@ import android.widget.Toast;
 
 import org.json.JSONArray;
 import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -33,13 +38,14 @@ public class NewsFeedActivity extends Activity {
     private ListView mListView;
     private ProgressBar mProgressView;
     private JSONArray notes;
+    private static JSONArray cInfo;
+    private static boolean[] checkedChildren;
     private boolean progress = false;
+    private static String[] childNames;
+    private String childIDList;
+    private static String IDs="", sMethod="";
 
     private ArrayList<ChildrenNotes> cNotes = new ArrayList<ChildrenNotes>();
-
-    public interface OnTaskFinished {
-        void setList();
-    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -48,69 +54,26 @@ public class NewsFeedActivity extends Activity {
         mListView = (ListView) findViewById(android.R.id.list);
         mProgressView = (ProgressBar) findViewById(R.id.progress_bar_news);
 
-        String[] childArray = null;
+
         JSONArray passedJSON=null;
         GetNotes note = new GetNotes();
+
         try{
 
             passedJSON = new JSONArray(this.getIntent().getStringExtra("JSONString"));
             if(passedJSON != null)
             {
 
-                String childIDList = passedJSON.getJSONObject(0).getString("children").replaceAll("\"", "");
+                childIDList = passedJSON.getJSONObject(0).getString("children").replaceAll("\"", "");
 
                 note.execute(childIDList);
+
             }
         }
         catch(JSONException e)
         {
             Log.e("JSON String: ", e.getMessage());
         }
-
-
-//        mListView = findViewById(R.id.activity_news_form);
-//        while(!progress)
-//        {
-//            showProgress(true);
-//        }
-
-
-//        final String [] testChildren = {"Johnny", "Jackie"};
-//        final String[] testNotes = {"Pooped Pants", "School Field Trip","Slapped Clarissa"};
-
-
-
-
-
-
-/*        ArrayAdapter<ArrayList<ChildrenNotes>> adapter = new ArrayAdapter<ArrayList<ChildrenNotes>>(this, R.layout.two_line_list, cNotes){
-            @Override
-            public View getView(int position, View convertView, ViewGroup parent){
-                View view = super.getView(position, convertView, parent);
-                TextView text1 = (TextView) view.findViewById(R.id.text1);
-                TextView text2 = (TextView) view.findViewById(R.id.text2);
-//                text1.setText(cNotes.get(position).getSubject());
-                text2.setText(cNotes.get(position).getMessage());
-                text1.setText("Hello, world!");
-                view.invalidate();
-//
-                return view;
-            }
-        };*/
-
-//       mListView.setAdapter(new ArrayAdapter<ChildrenNotes>(this, R.layout.activity_news_feed, android.R.id.text1, cNotes) {
-//           @Override
-//           public View getView(int position, View convertView, ViewGroup parent) {
-//
-//               View view = super.getView(position, convertView, parent);
-//               TextView text1 = (TextView) view.findViewById(android.R.id.text1);
-//               TextView text2 = (TextView) view.findViewById(android.R.id.text2);
-//               text1.setText(cNotes.get(position).getSubject());
-//               text2.setText(cNotes.get(position).getMessage());
-//
-//               return view;
-//           }
-//       });
 
 
     }
@@ -134,8 +97,15 @@ public class NewsFeedActivity extends Activity {
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
-        if (id == R.id.action_settings) {
+        if (id == R.id.action_settings)
+        {
             return true;
+        }
+        else if(id == R.id.action_sign_in)
+        {
+
+            GetChildInfo info = new GetChildInfo();
+            info.execute(childIDList);
         }
         return super.onOptionsItemSelected(item);
     }
@@ -152,8 +122,7 @@ public class NewsFeedActivity extends Activity {
             BufferedReader reader = null;
             String jsonStr = "";
 
-//            for(String s : params[0])
-//            {
+
                 try
                 {
                     Uri builtUri = Uri.parse(BASE_URL).buildUpon()
@@ -183,7 +152,6 @@ public class NewsFeedActivity extends Activity {
                         if (buffer.length() != 0)
                         {
                             jsonStr += buffer.toString();
-//                            Log.v("JSON String: ", jsonStr);
                         }
                     }
                 }
@@ -207,7 +175,6 @@ public class NewsFeedActivity extends Activity {
                         Log.e("Error closing stream", e.getMessage());
                     }
                 }
-//            }
 
             try
             {
@@ -234,7 +201,6 @@ public class NewsFeedActivity extends Activity {
                     cNotes.add(c);
 
                 }
-                cNotes.add(new ChildrenNotes("7", "101", "Michael is a pain in my ass.", "2", "1"));
 
                 return true;
             }
@@ -292,5 +258,299 @@ public class NewsFeedActivity extends Activity {
             mListView.setVisibility(show ? View.GONE : View.VISIBLE);
         }
     }
+    public static class SignInOutDialog extends DialogFragment {
+
+        public Dialog onCreateDialog(Bundle savedInstanceState) {
+
+
+            AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+            builder.setTitle(R.string.add_note_title)
+                    .setMultiChoiceItems(childNames, checkedChildren, new DialogInterface.OnMultiChoiceClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialogInterface, int i, boolean b) {
+                            checkedChildren[i] = b;
+                        }
+                    })
+                    .setPositiveButton("Sign In", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialogInterface, int i) {
+
+                            ArrayList<String> temp = new ArrayList<String>();
+                            IDs = "[";
+                            try {
+                                for (int j = 0; j < checkedChildren.length; ++j) {
+                                    if (checkedChildren[j]) {
+                                        temp.add(cInfo.getJSONObject(j).getString("childID"));
+                                    }
+                                }
+                                for(int j=0; j<temp.size(); ++j)
+                                {
+                                    IDs += temp.get(j);
+                                    if(j<temp.size()-1)
+                                    {
+                                        IDs += ",";
+                                    }
+                                }
+                                IDs += "]";
+                                sMethod = "signin";
+                            } catch (JSONException e) {
+                                Log.e("JSON", e.getMessage());
+                            }
+                        }
+                    })
+                    .setNegativeButton("Sign Out", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialogInterface, int i) {
+                            ArrayList<String> temp = new ArrayList<String>();
+                            IDs = "[";
+                            try {
+                                for (int j = 0; j < checkedChildren.length; ++j) {
+                                    if (checkedChildren[j]) {
+                                        temp.add(cInfo.getJSONObject(j).getString("attendID"));
+                                    }
+                                }
+                                for(int j=0; j<temp.size(); ++j)
+                                {
+                                    IDs += temp.get(j);
+                                    if(j<temp.size()-1)
+                                    {
+                                        IDs += ",";
+                                    }
+                                }
+                                IDs += "]";
+                                sMethod = "signout";
+                            } catch (JSONException e) {
+                                Log.e("JSON", e.getMessage());
+                            }
+
+                        }
+                    });
+            return builder.create();
+        }
+        public void onDismiss(DialogInterface dialogInterface)
+        {
+
+            ((NewsFeedActivity)getActivity()).callSignAsync(IDs, sMethod);
+        }
+    }
+
+    public void callSignAsync(String IDs, String sMethod)
+    {
+        SignInOut signInOut = new SignInOut();
+        Log.v("Sign", "Sign " + sMethod);
+        signInOut.execute(IDs, sMethod);
+    }
+
+    public class GetChildInfo extends AsyncTask<String, Void, Boolean> {
+
+        protected Boolean doInBackground(String... params) {
+
+            final String BASE_URL = "http://davisengeler.gwdnow.com/child.php?getinfo";
+            final String CHILD_ID = "childids";
+            HttpURLConnection urlConnection = null;
+            BufferedReader reader = null;
+            String jsonStr = "";
+
+
+            try {
+                Uri builtUri = Uri.parse(BASE_URL).buildUpon()
+                        .appendQueryParameter(CHILD_ID, params[0]).build();
+
+                Log.v("TEST:   ", builtUri.toString());
+
+                URL url = new URL(builtUri.toString());
+
+                urlConnection = (HttpURLConnection) url.openConnection();
+                urlConnection.setRequestMethod("GET");
+                urlConnection.connect();
+
+                // Read the input stream into a String
+                InputStream inputStream = urlConnection.getInputStream();
+                StringBuffer buffer = new StringBuffer();
+                if (inputStream != null) {
+                    reader = new BufferedReader(new InputStreamReader(inputStream));
+
+                    String line;
+                    while ((line = reader.readLine()) != null) {
+                        //makes easy to read in logs
+                        buffer.append(line + "\n");
+                    }
+                    if (buffer.length() != 0) {
+                        jsonStr += buffer.toString();
+                    }
+                }
+            } catch (MalformedURLException e) {
+                Log.e("URL Error: ", e.getMessage());
+            } catch (IOException e) {
+                Log.e("Connection: ", e.getMessage());
+            } finally {
+                urlConnection.disconnect();
+                try {
+                    if (reader != null)
+                        reader.close();
+                } catch (IOException e) {
+                    Log.e("Error closing stream", e.getMessage());
+                }
+            }
+
+            try {
+
+                cInfo = new JSONArray(jsonStr);
+                childNames = new String[cInfo.length()];
+
+                for(int i=0; i<childNames.length; ++i)
+                {
+                    childNames[i] = cInfo.getJSONObject(i).getString("firstName") +
+                            cInfo.getJSONObject(i).getString("lastName");
+                }
+                checkedChildren = new boolean[childNames.length];
+
+                try
+                {
+                    for(int i=0; i<checkedChildren.length; ++i)
+                    {
+                        Log.v("Attend status: ", cInfo.getJSONObject(i).getString("attendID"));
+                        if(cInfo.getJSONObject(i).getString("attendID").compareTo("null")==0)
+                        {
+                            checkedChildren[i] = false;
+                        }
+                        else
+                        {
+                            checkedChildren[i] = true;
+                        }
+
+                    }
+                }catch(JSONException e)
+                {
+                    Log.e("JSON ERROR", e.getMessage());
+                }
+
+
+                return true;
+
+
+            } catch (JSONException e) {
+                Log.e("JSON Error: ", e.getMessage());
+            }
+
+            return false;
+        }
+
+        protected void onPostExecute(Boolean success){
+            if(success)
+            {
+                DialogFragment s = new SignInOutDialog();
+                s.show(getFragmentManager(), "sign");
+
+
+            }
+            else
+            {
+                Toast.makeText(getApplicationContext(), "FUCK YOU", Toast.LENGTH_LONG).show();
+            }
+
+        }
+
+    }
+
+    public class SignInOut extends AsyncTask<String, Void, Boolean> {
+        JSONObject statement;
+        protected Boolean doInBackground(String... params) {
+            boolean success = false;
+            String BASE_URL, ID;
+            if(sMethod.compareTo("signin")==0)
+            {
+                BASE_URL = "http://davisengeler.gwdnow.com/child.php?signin";
+                ID = "childids";
+            }
+            else
+            {
+                BASE_URL = "http://davisengeler.gwdnow.com/child.php?signout";
+                ID = "attendids";
+            }
+            HttpURLConnection urlConnection = null;
+            BufferedReader reader = null;
+            String jsonStr = "";
+
+
+            try {
+                Uri builtUri = Uri.parse(BASE_URL).buildUpon()
+                        .appendQueryParameter(ID, params[0]).build();
+
+                Log.v("TEST:   ", builtUri.toString());
+
+                URL url = new URL(builtUri.toString());
+
+                urlConnection = (HttpURLConnection) url.openConnection();
+                urlConnection.setRequestMethod("GET");
+                urlConnection.connect();
+
+                // Read the input stream into a String
+                InputStream inputStream = urlConnection.getInputStream();
+                StringBuffer buffer = new StringBuffer();
+                if (inputStream != null) {
+                    reader = new BufferedReader(new InputStreamReader(inputStream));
+
+                    String line;
+                    while ((line = reader.readLine()) != null) {
+                        //makes easy to read in logs
+                        buffer.append(line + "\n");
+                    }
+                    if (buffer.length() != 0) {
+                        jsonStr += buffer.toString();
+                    }
+                }
+            } catch (MalformedURLException e) {
+                Log.e("URL Error: ", e.getMessage());
+            } catch (IOException e) {
+                Log.e("Connection: ", e.getMessage());
+            } finally {
+                urlConnection.disconnect();
+                try {
+                    if (reader != null)
+                        reader.close();
+                } catch (IOException e) {
+                    Log.e("Error closing stream", e.getMessage());
+                }
+            }
+
+            try {
+                statement = new JSONObject(jsonStr);
+                success = statement.getBoolean("successful");
+
+            } catch (JSONException e) {
+                Log.e("JSON Error: ", e.getMessage());
+            }
+
+            return success;
+        }
+
+        protected void onPostExecute(Boolean success)
+        {
+            if(success)
+            {
+                try
+                {
+                    Toast.makeText(getApplicationContext(), statement.getString("statusMessage"), Toast.LENGTH_LONG).show();
+                }catch (JSONException e)
+                {
+                    Log.v("JSON", e.getMessage());
+                }
+
+            }
+            else
+            {
+                try
+                {
+                    Toast.makeText(getApplicationContext(), statement.getString("statusMessage"), Toast.LENGTH_LONG).show();
+                }catch (JSONException e)
+                {
+                    Log.v("JSON", e.getMessage());
+                }
+            }
+        }
+
+    }
+
 }
 
