54d0123034c445397d7eafee65e847632bb4abeb
Davis Engeler <davisengeler+github@gmail.com>
2014-11-25 13:29:11 -0500

    Working on push notifications

--- src/Server/testnotification.php
+++ src/Server/testnotification.php
@@ -1,6 +1,11 @@
 <?php
 
   include('config.php');
+
+  echo "Hold on.<br />";
+
   sendPushNotification(array("APA91bF2_TLLr01h8ppMkkwdpZN4mxJ--IynAGaPREeOpccNjB2GjPeM8M6os7jQ6Mhh5zQ4vTs2PBwGukVaCCwEw6Qg9hn_G1uxV1G2tX3EeyWoTyUdhwPMKlkPQhod9fZk5MyeO9dblP5vmNqAdSDYo1ubuAohzac_6UV95r-57NmSjEdU46I"));
+
+  echo "It should have been done.";
 
 <?
