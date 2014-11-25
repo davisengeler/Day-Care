b0184eb75fe8c126f79086742fe685186bd8466f
Davis Engeler <davisengeler+github@gmail.com>
2014-11-25 13:27:09 -0500

    Working on push notifications

---
+++ src/Server/testnotification.php
@@ -0,0 +1,5 @@
+<?php
+
+  sendPushNotification(array("APA91bF2_TLLr01h8ppMkkwdpZN4mxJ--IynAGaPREeOpccNjB2GjPeM8M6os7jQ6Mhh5zQ4vTs2PBwGukVaCCwEw6Qg9hn_G1uxV1G2tX3EeyWoTyUdhwPMKlkPQhod9fZk5MyeO9dblP5vmNqAdSDYo1ubuAohzac_6UV95r-57NmSjEdU46I"));
+
+<?
