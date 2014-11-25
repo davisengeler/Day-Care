9a59f3b8c2f4e666718106dcdb97cc16d0ccef19
Davis Engeler <davisengeler+github@gmail.com>
2014-11-25 13:05:18 -0500

    Working on push notifications

--- src/Server/config.php
+++ src/Server/config.php
@@ -1,5 +1,34 @@
 <?php
 
+  // Push Notification Info
+  define("GCM_SenderID", "385041079398");
+  define("GCM_APIKey", "AIzaSyCjXiSeTMvzc9uqDa9hP5sYsslSZXQpKlY");
+
+  sendPushNotification(array("123", "321"));
+
+  function sendPushNotification($gcmRegistrationIDs)
+  {
+    $url = "https://android.googleapis.com/gcm/send";
+
+    $data = array(
+      'registration_ids'  => $gcmRegistrationIDs,
+      'collapse_key'      => "note"
+    );
+
+    $options = array(
+      'http' => array(
+        'header'  => "Content-Type:application/json\r\nAuthorization:key=" . GCM_APIKey,
+        'method'  => 'POST',
+        'content' => json_encode($data),
+      ),
+    );
+
+    $context  = stream_context_create($options);
+    $result = file_get_contents($url, false, $context);
+
+    var_dump($result);
+  }
+
   // Database Configuration
   define("Database_NAME", "davisengeler_daycare");
   define("Database_HOST", "localhost");
@@ -10,7 +39,7 @@
   {
     $connection = mysqli_connect(Database_HOST, Database_USER, Database_PASS, Database_NAME) or die("Could not connect to database");
     return $connection;
-  }  
+  }
 
   // Provides a check to make sure the device making calls to the API is a verified device
   function deviceIsVerified($database, $deviceID)
