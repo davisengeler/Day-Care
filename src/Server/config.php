8a7c387f2c34cc6723f4d4105d0cff26aabdf5be
Davis Engeler <davisengeler+github@gmail.com>
2014-11-25 13:14:49 -0500

    Working on push notifications

--- src/Server/config.php
+++ src/Server/config.php
@@ -8,25 +8,30 @@
 
   function sendPushNotification($gcmRegistrationIDs)
   {
-    $url = "https://android.googleapis.com/gcm/send";
-
-    $data = array(
-      'registration_ids'  => $gcmRegistrationIDs,
+    $apiKey = GCM_APIKey;
+    $registrationIDs = $gcmRegistrationIDs;
+    $url = 'https://android.googleapis.com/gcm/send';
+    $fields = array(
+      'registration_ids'  => $registrationIDs,
       'collapse_key'      => "note"
     );
-
-    $options = array(
-      'http' => array(
-        'header'  => "Content-Type:application/json\r\nAuthorization:key=" . GCM_APIKey,
-        'method'  => 'POST',
-        'content' => http_build_query($data),
-      ),
+    $headers = array(
+      'Authorization: key=' . $apiKey,
+      'Content-Type: application/json'
     );
+    $ch = curl_init();
+    curl_setopt( $ch, CURLOPT_URL, $url );
+    curl_setopt( $ch, CURLOPT_POST, true );
+    curl_setopt( $ch, CURLOPT_HTTPHEADER, $headers);
+    curl_setopt( $ch, CURLOPT_RETURNTRANSFER, true );
+    curl_setopt ($ch, CURLOPT_SSL_VERIFYHOST, 0);
+    curl_setopt ($ch, CURLOPT_SSL_VERIFYPEER, 0);
+    curl_setopt( $ch, CURLOPT_POSTFIELDS, json_encode($fields) );
 
-    $context  = stream_context_create($options);
-    $result = file_get_contents($url, false, $context);
-
-    var_dump($result);
+    $result = curl_exec($ch);
+    if(curl_errno($ch)){ echo 'Curl error: ' . curl_error($ch); }
+    curl_close($ch);
+    echo $result;
   }
 
   // Database Configuration
